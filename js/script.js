fc7cd999b2471a9fb430d2df430ff27adf8315f7
kzim <k.zimny1994@gmail.com>
2022-07-06 01:59:36 -0500

    Set up global vars

---
+++ js/script.js
@@ -0,0 +1,27 @@
+// Global Variables
+
+//ul for guessed letters
+const guessedLetters = document.querySelector(".guessed-letters");
+//guess button
+const guessButton = document.querySelector(".guess");
+//guess text input
+const guessInput = document.querySelector(".letter");
+//word in progress
+const wordInProgress = document.querySelector(".word-in-progress");
+//remaining guesses
+const remainingGuesses = document.querySelector(".remaining");
+//span inside remaining guesses
+const remainingGuessesSpan = document.querySelector(".remaining span");
+//message on letter guess
+const guessMessage = document.querySelector(".message");
+//play again btn
+const playAgainButton = document.querySelector(".play-again");
+//testing word
+const word = "magnolia";
+
+// ------------------------------------------------------------------------------
+// Function to Add Placeholders for Each Letter
+
+wordInProgress = function (word) {
+  word.length;
+};
