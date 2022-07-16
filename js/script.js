7fb8b9bc4d19f7f396e0f374e67a4e87c82eee94
kzim <k.zimny1994@gmail.com>
2022-07-16 09:44:02 -0500

    Added guessing feature

--- js/script.js
+++ js/script.js
@@ -16,9 +16,12 @@ const message = document.querySelector(".message");
 const playAgainButton = document.querySelector(".play-again");
 //test word
 const word = "magnolia";
+//guessed letters
+const guessedLetters = [];
 //-------------------------------------------------
 
 // Display our symbols as placeholders for the chosen word's letters
+//-------------------------------------------------
 const placeholder = function (word) {
   const placeholderLetters = [];
   for (const letter of word) {
@@ -32,10 +35,81 @@ const placeholder = function (word) {
 placeholder(word);
 
 // Button event listener
-
+//-------------------------------------------------
 guessLetterButton.addEventListener("click", function (e) {
   e.preventDefault();
   const textInputGuess = letterInput.value;
-  console.log(textInputGuess);
+  // console.log(textInputGuess);
+  const goodGuess = validateInput(textInputGuess);
+  if (goodGuess) {
+    makeGuess(textInputGuess);
+  }
   letterInput.value = "";
 });
+
+//check player input
+//-------------------------------------------------
+
+const validateInput = function (input) {
+  const acceptedLetter = /[a-zA-Z]/;
+  if (input.length === 0) {
+    // Is the input empty?
+    message.innerText = "Please enter a letter.";
+  } else if (input.length > 1) {
+    // Did you type more than one letter?
+    message.innerText = "Please enter a single letter.";
+  } else if (!input.match(acceptedLetter)) {
+    // Did you type a number, a special character or some other non letter thing?
+    message.innerText = "Please enter a letter from A to Z.";
+  } else {
+    // We finally got a single letter, omg yay
+    return input;
+  }
+};
+
+const makeGuess = function (guess) {
+  guess = guess.toUpperCase();
+  if (guessedLetters.includes(guess)) {
+    message.innerText = "You already guessed that letter, silly. Try again.";
+  } else {
+    guessedLetters.push(guess);
+    // console.log(guessedLetters);
+    playerGuesses();
+    updateWordInProgress(guessedLetters);
+  }
+};
+
+//show guessed letters
+//-------------------------------------------------
+
+const playerGuesses = function () {
+  guessedLettersElement.innerHTML = "";
+  for (const letter of guessedLetters) {
+    const li = document.createElement("li");
+    li.innerText = letter;
+    guessedLettersElement.append(li);
+  }
+};
+
+const updateWordInProgress = function (guessedLetters) {
+  const wordUpper = word.toUpperCase();
+  const wordArray = wordUpper.split("");
+  const revealWord = [];
+  for (const letter of wordArray) {
+    if (guessedLetters.includes(letter)) {
+      revealWord.push(letter.toUpperCase());
+    } else {
+      revealWord.push("●");
+    }
+  }
+  // console.log(revealWord);
+  wordInProgress.innerText = revealWord.join("");
+  checkIfWin();
+};
+
+const checkIfWin = function () {
+  if (word.toUpperCase() === wordInProgress.innerText) {
+    message.classList.add("win");
+    message.innerHTML = `<p class="highlight">You guessed the correct word! Congrats!</p>`;
+  }
+};
