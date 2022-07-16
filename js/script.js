a96e01ac53a31a6931f3584e9ebc83ed7717a602
kzim <k.zimny1994@gmail.com>
2022-07-16 09:01:36 -0500

    Updated step 1

--- js/script.js
+++ js/script.js
@@ -1,99 +1,41 @@
-// Global Variables
-
-//ul for guessed letters
-const guessedLetters = document.querySelector(".guessed-letters");
-//guess button
-const guessButton = document.querySelector(".guess");
-//guess text input
-const guessInput = document.querySelector(".letter");
-//word in progress
+//Letters that the user has guessed
+const guessedLettersElement = document.querySelector(".guessed-letters");
+//guess button for playing game
+const guessLetterButton = document.querySelector(".guess");
+//input for letter
+const letterInput = document.querySelector(".letter");
+//the current word in progress
 const wordInProgress = document.querySelector(".word-in-progress");
-//remaining guesses
-const remainingGuesses = document.querySelector(".remaining");
-//span inside remaining guesses
+//Amount of remaining guesses left
+const remainingGuessesElement = document.querySelector(".remaining");
+//span for amount of remaining guesses
 const remainingGuessesSpan = document.querySelector(".remaining span");
-//message on letter guess
-const guessMessage = document.querySelector(".message");
-//play again btn
+//game message
+const message = document.querySelector(".message");
+//play again button
 const playAgainButton = document.querySelector(".play-again");
-//testing word
+//test word
 const word = "magnolia";
-//guessed letters array that the player has guessed
-const guessedLettersArray = [];
+//-------------------------------------------------
 
-// ------------------------------------------------------------------------------
-// Function to Add Placeholders for Each Letter
-
-const updateWordsInProgress = function (word) {
-  const wordArray = word.split("");
-  // console.log(wordArray);
-  for (let index = 0; index < wordArray.length; index++) {
-    wordArray[index] = "●";
+// Display our symbols as placeholders for the chosen word's letters
+const placeholder = function (word) {
+  const placeholderLetters = [];
+  for (const letter of word) {
+    // console.log(letter);
+    placeholderLetters.push("●");
+    // console.log(placeholderLetters);
   }
-  // console.log(wordArray);
-  wordInProgress.innerText = wordArray.join("");
+  wordInProgress.innerText = placeholderLetters.join("");
 };
 
-updateWordsInProgress(word);
+placeholder(word);
 
-// ------------------------------------------------------------------------------
-// Button Event listener & Key value capture
+// Button event listener
 
-guessButton.addEventListener("click", function (e) {
+guessLetterButton.addEventListener("click", function (e) {
   e.preventDefault();
-  const key = guessInput.value;
-  guessInput.value = "";
-  // console.log(key);
-
-  if (validatePlayerInput(key)) {
-    console.log(key);
-    makeGuess(key);
-  }
+  const textInputGuess = letterInput.value;
+  console.log(textInputGuess);
+  letterInput.value = "";
 });
-
-// ------------------------------------------------------------------------------
-// Check Players input
-
-const validatePlayerInput = function (input) {
-  const acceptedLetters = /[a-zA-Z]/;
-
-  if (input === "") {
-    console.log("Input is empty");
-    guessMessage.innerText = "Input is empty";
-  } else if (input.length > 1) {
-    console.log("Please only input one character");
-    guessMessage.innerText = "Please only input one character";
-  } else if (!input.match(acceptedLetters)) {
-    console.log("Please enter acceptable letters");
-    guessMessage.innerText = "Please enter acceptable letters";
-  } else {
-    return input;
-  }
-};
-
-// ------------------------------------------------------------------------------
-// Function to capture input
-
-const makeGuess = function (letter) {
-  const uppercaseLetter = letter.toUpperCase();
-  if (guessedLettersArray.includes(uppercaseLetter)) {
-    console.log("Already includes this letter");
-    guessMessage.innerText = "Already includes this letter";
-  } else {
-    guessedLettersArray.push(uppercaseLetter);
-    console.log(guessedLettersArray);
-    playersGuesses();
-  }
-};
-
-// ------------------------------------------------------------------------------
-// Player's guesses
-
-const playersGuesses = function () {
-  guessedLetters.innerHTML = "";
-  for (const list of guessedLettersArray) {
-    const li = document.createElement("li");
-    li.innerHTML = list;
-    guessedLetters.append(li);
-  }
-};
